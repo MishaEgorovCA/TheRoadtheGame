1d110e5fe523618aa26efd9e3314d7a8baa1a027
Misha Egorov <misha.a.egorov@gmail.com>
2020-03-04 06:50:36 -0800

    Update 2 items.

--- game.js
+++ game.js
@@ -1,3 +1,9 @@
-function newCard(text,choices) {
+var hunger = 50;
+var health = 50;
+var sanity = 50;
+
+var scenarios = JSON.parse();
+
+function newCard(text, choices) {
     var cardHtml = document.getElementById("").innerHTML;
 }
