da5477f13757edd25bfe200f772b53b1735cac1d
Misha Egorov <misha.a.egorov@gmail.com>
2020-03-06 09:53:23 -0800

    Updated health / hunger animation to not go past 100

--- game.js
+++ game.js
@@ -12,7 +12,7 @@ function choice(cID) {
     if (hunger <= 0) { endGame("you loose because you starved to death"); }
     else if (hunger >= 100) { endGame("you loose because you got too damn fat"); }
     else if (health <= 0) { endGame("you loose because you got too sick"); }
-    else if (health >= 100) { endGame("you loose because you were too health????? maybe some bad guys see your good health and eat your fresh meat up? idk"); }
+    else if (health >= 100) { endGame("you loose because you were too healthy????? maybe some bad guys see your good health and eat your fresh meat up? idk"); }
     else if (sanity <= 0) { endGame("you loose because you went insane"); }
     else if (sanity >= 100) { endGame("you loose because you fall into a state of bliss and stop caring about survival believing everything is great, and die."); }
     else {
@@ -25,7 +25,7 @@ function choice(cID) {
 
 function endGame(reason) {
     document.getElementById("card").remove();
-    setTimeout(endScreen,3000);
+    setTimeout(endScreen,2500);
     function endScreen() {
         alert(reason);
         location.reload();
@@ -71,21 +71,20 @@ function changeStats(changes = []) {
 
     var interval = setInterval(frame, 25);
     function frame() {
-        if (stats[0] === tStats[0] && stats[1] === tStats[1] && stats[2] === tStats[2]) {
-            clearInterval(interval);
-        } else {
+            var done = 0;
             stats.forEach((stat, i) => {
-                if (stats[i] !== tStats[i]) {
+                if (stats[i] !== tStats[i] && stats[i] > 0 && stats[i] < 100) {
                     if (stats[i] < tStats[i]) {
                         stats[i]++;
                     } else {
                         stats[i]--;
                     }
                     elems[i].style.width = `${stats[i]}%`;
-                }
+                } else { done++; }
             });
-
-        }
+            if (done === 3) {
+                clearInterval(interval);
+            }
     }
 }
 
