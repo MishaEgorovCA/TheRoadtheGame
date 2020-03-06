118d84c0e7844755e65031e1215bf27a5e2dbf52
Misha Egorov <misha.a.egorov@gmail.com>
2020-03-06 08:54:41 -0800

    New easier data format (the lazy way)

---
+++ resources/scenarios.js
@@ -0,0 +1,96 @@
+const rawData = `
+{
+    "scenarios": [
+        {
+            "text": "test",
+            "image": "",
+            "choices": [
+                {
+                    "text": "",
+                    "affects": [0, 0, 0],
+                    "unlocks": false,
+                    "locks": false,
+                    "chained": false
+                },
+                {
+                    "text": "",
+                    "affects": [0, 0, 0],
+                    "unlocks": false,
+                    "locks": false,
+                    "chained": false
+                },
+                {
+                    "text": "",
+                    "affects": [0, 0, 0],
+                    "unlocks": false,
+                    "locks": false,
+                    "chained": false
+                }
+            ],
+            "repeatable": false,
+            "story": false,
+            "locked": false
+        },
+        {
+            "text": "",
+            "image": "",
+            "choices": [
+                {
+                    "text": "",
+                    "affects": [0, 0, 0],
+                    "unlocks": false,
+                    "locks": false,
+                    "chained": false
+                },
+                {
+                    "text": "",
+                    "affects": [0, 0, 0],
+                    "unlocks": false,
+                    "locks": false,
+                    "chained": false
+                },
+                {
+                    "text": "",
+                    "affects": [0, 0, 0],
+                    "unlocks": false,
+                    "locks": false,
+                    "chained": false
+                }
+            ],
+            "repeatable": false,
+            "story": false,
+            "locked": false
+        },
+        {
+            "text": "",
+            "image": "",
+            "choices": [
+                {
+                    "text": "",
+                    "affects": [0, 0, 0],
+                    "unlocks": false,
+                    "locks": false,
+                    "chained": false
+                },
+                {
+                    "text": "",
+                    "affects": [0, 0, 0],
+                    "unlocks": false,
+                    "locks": false,
+                    "chained": false
+                },
+                {
+                    "text": "",
+                    "affects": [0, 0, 0],
+                    "unlocks": false,
+                    "locks": false,
+                    "chained": false
+                }
+            ],
+            "repeatable": false,
+            "story": false,
+            "locked": false
+        }
+    ]
+}
+`;
