7fdcbffa5deef46efad01b860bde1866dc0f8897
Misha Egorov <misha.a.egorov@gmail.com>
2020-03-06 09:43:48 -0800

    Update game kinda works now.

--- game.js
+++ game.js
@@ -2,26 +2,36 @@ var hunger = 50;
 var health = 50;
 var sanity = 50;
 
-/*var rawFile = new XMLHttpRequest();
-rawFile.open("GET", "resources/scenarios.json", false);
-rawFile.onreadystatechange = function ()
-{
-    if(rawFile.readyState === 4)
-    {
-        if(rawFile.status === 200 || rawFile.status == 0)
-        {
-            var allText = rawFile.responseText;
-            alert(allText);
-        }
-    }
-}
-rawFile.send(null);
+var currentScenario = 0;
 
-var scenarios = JSON.parse(rawFile).scenarios;*/
+var scenarios = JSON.parse(rawData).scenarios;
 
 function choice(cID) {
+    changeStats(scenarios[currentScenario].choices[cID].affects);
 
+    if (hunger <= 0) { endGame("you loose because you starved to death"); }
+    else if (hunger >= 100) { endGame("you loose because you got too damn fat"); }
+    else if (health <= 0) { endGame("you loose because you got too sick"); }
+    else if (health >= 100) { endGame("you loose because you were too health????? maybe some bad guys see your good health and eat your fresh meat up? idk"); }
+    else if (sanity <= 0) { endGame("you loose because you went insane"); }
+    else if (sanity >= 100) { endGame("you loose because you fall into a state of bliss and stop caring about survival believing everything is great, and die."); }
+    else {
+
+
+        newCard(currentScenario + 1);
+        currentScenario++;
+    }
 }
+
+function endGame(reason) {
+    document.getElementById("card").remove();
+    setTimeout(endScreen,3000);
+    function endScreen() {
+        alert(reason);
+        location.reload();
+    }
+}
+
 
 function newCard(sID) {
     var cardElem = document.getElementById("card");
@@ -34,6 +44,18 @@ function newCard(sID) {
     var choiceElem = document.getElementById("cardChoices");
 
     infoElem.innerHTML = `<p>${scenarios[sID].text}</p>`;
+
+    if (scenarios[sID].image) {
+        //IMAGE FORMAT
+    }
+
+    choiceElem.innerHTML = "";
+    scenarios[sID].choices.forEach((choice, i) => {
+        choiceElem.innerHTML += `
+            <p class="w3-btn w3-margin-top w3-left-align w3-ripple w3-block" onclick="choice(${i});">
+            ${choice.text}
+            </p>`;
+    });
 }
 
 function changeStats(changes = []) {
@@ -52,7 +74,7 @@ function changeStats(changes = []) {
         if (stats[0] === tStats[0] && stats[1] === tStats[1] && stats[2] === tStats[2]) {
             clearInterval(interval);
         } else {
-            stats.forEach((stat,i) => {
+            stats.forEach((stat, i) => {
                 if (stats[i] !== tStats[i]) {
                     if (stats[i] < tStats[i]) {
                         stats[i]++;
@@ -60,10 +82,11 @@ function changeStats(changes = []) {
                         stats[i]--;
                     }
                     elems[i].style.width = `${stats[i]}%`;
-                } 
+                }
             });
 
         }
     }
 }
 
+newCard(0);
--- resources/scenarios.js
+++ resources/scenarios.js
@@ -1,96 +1,103 @@
 const rawData = `
 {
-    "scenarios": [
+  "scenarios": [
+    {
+      "text": "Test scenario 1",
+      "image": "",
+      "choices": [
         {
-            "text": "test",
-            "image": "",
-            "choices": [
-                {
-                    "text": "",
-                    "affects": [0, 0, 0],
-                    "unlocks": false,
-                    "locks": false,
-                    "chained": false
-                },
-                {
-                    "text": "",
-                    "affects": [0, 0, 0],
-                    "unlocks": false,
-                    "locks": false,
-                    "chained": false
-                },
-                {
-                    "text": "",
-                    "affects": [0, 0, 0],
-                    "unlocks": false,
-                    "locks": false,
-                    "chained": false
-                }
-            ],
-            "repeatable": false,
-            "story": false,
-            "locked": false
+          "text": "+10 +40 +25",
+          "affects": [ 10, 40, 25 ],
+          "unlocks": false,
+          "locks": false,
+          "chained": false
         },
         {
-            "text": "",
-            "image": "",
-            "choices": [
-                {
-                    "text": "",
-                    "affects": [0, 0, 0],
-                    "unlocks": false,
-                    "locks": false,
-                    "chained": false
-                },
-                {
-                    "text": "",
-                    "affects": [0, 0, 0],
-                    "unlocks": false,
-                    "locks": false,
-                    "chained": false
-                },
-                {
-                    "text": "",
-                    "affects": [0, 0, 0],
-                    "unlocks": false,
-                    "locks": false,
-                    "chained": false
-                }
-            ],
-            "repeatable": false,
-            "story": false,
-            "locked": false
+          "text": "-49 -49 -49",
+          "affects": [ -49, -49, -49 ],
+          "unlocks": false,
+          "locks": false,
+          "chained": false
         },
         {
-            "text": "",
-            "image": "",
-            "choices": [
-                {
-                    "text": "",
-                    "affects": [0, 0, 0],
-                    "unlocks": false,
-                    "locks": false,
-                    "chained": false
-                },
-                {
-                    "text": "",
-                    "affects": [0, 0, 0],
-                    "unlocks": false,
-                    "locks": false,
-                    "chained": false
-                },
-                {
-                    "text": "",
-                    "affects": [0, 0, 0],
-                    "unlocks": false,
-                    "locks": false,
-                    "chained": false
-                }
-            ],
-            "repeatable": false,
-            "story": false,
-            "locked": false
+          "text": "+5 -5 +5",
+          "affects": [ 5, -5, 5 ],
+          "unlocks": false,
+          "locks": false,
+          "chained": false
         }
-    ]
+      ],
+      "repeatable": false,
+      "story": false,
+      "locked": false
+    },
+    {
+      "text": "Test scenario 2",
+      "image": "",
+      "choices": [
+        {
+          "text": "-100 0 0",
+          "affects": [ -100, 0, 0 ],
+          "unlocks": false,
+          "locks": false,
+          "chained": false
+        },
+        {
+          "text": "0 -100 0",
+          "affects": [ 0, -100, 0 ],
+          "unlocks": false,
+          "locks": false,
+          "chained": false
+        },
+        {
+          "text": "0 0 -100",
+          "affects": [ 0, 0, -100 ],
+          "unlocks": false,
+          "locks": false,
+          "chained": false
+        },
+        {
+          "text": "0 0 0",
+          "affects": [ 0, 0, 0 ],
+          "unlocks": false,
+          "locks": false,
+          "chained": false
+        }
+      ],
+      "repeatable": false,
+      "story": false,
+      "locked": false
+    },
+    {
+      "text": "Test scenario 3",
+      "image": "",
+      "choices": [
+        {
+          "text": "+100 0 0",
+          "affects": [ 100, 0, 0 ],
+          "unlocks": false,
+          "locks": false,
+          "chained": false
+        },
+        {
+          "text": "0 +100 0",
+          "affects": [ 0, 100, 0 ],
+          "unlocks": false,
+          "locks": false,
+          "chained": false
+        },
+        {
+          "text": "0 0 +100",
+          "affects": [ 0, 0, 100 ],
+          "unlocks": false,
+          "locks": false,
+          "chained": false
+        }
+      ],
+      "repeatable": false,
+      "story": false,
+      "locked": false
+    }
+  ]
 }
 `;
